5ebc0f03f815f4bf35d69240333ba29129deb53b
Balram Pandey <balrampandey19@gmail.com>
2017-04-20 16:05:39 +0530

    permission fix

--- app/src/main/java/com/redcarpet/googlevision_android/Camera/CameraSourcePreview.java
+++ app/src/main/java/com/redcarpet/googlevision_android/Camera/CameraSourcePreview.java
@@ -1,7 +1,10 @@
 package com.redcarpet.googlevision_android.Camera;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.support.v4.app.ActivityCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
@@ -63,6 +66,16 @@ public class CameraSourcePreview extends ViewGroup {
 
     private void startIfReady() throws IOException {
         if (mStartRequested && mSurfaceAvailable) {
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             mCameraSource.start(mSurfaceView.getHolder());
             if (mOverlay != null) {
                 Size size = mCameraSource.getPreviewSize();
